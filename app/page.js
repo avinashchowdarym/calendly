566c08842d22d02a2dc4a3d4b8ddcb9dd2ab711b
mavinashchowdary <mavinashchowdaryofficial@gmail.com>
2025-03-24 22:46:56 +0530

    added a few

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 
 export default function Home() {
   return (
     <div>
-      HI THERE
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
     </div>
   );
 }
